6da20bfecd1720266593bc2d11bae1995821df38
slarrarte <slarrarte@gmail.com>
2024-03-30 16:32:01 -0400

    Added <edit-config> function to netconfActions.py

--- netconfActions.py
+++ netconfActions.py
@@ -1,7 +1,7 @@
 from ncclient import manager
 import xml.dom.minidom
 
-# Get Capabilities function
+# Get Capabilities function (aka <hello> rpc)
 def netconfGetCapabilities(
         host,
         port,
@@ -61,6 +61,33 @@ def netconfGet(
         device_params={'name': ios}
     ) as m:
         netconfReply = m.get_config(filter)
+        # Make returned XML data more human-readable
+        temp = xml.dom.minidom.parseString(str(netconfReply.xml))
+        new_xml = temp.toprettyxml()
+        print(new_xml)
+
+# <edit-config>
+def netconfEditConfig(
+        host,
+        port,
+        username,
+        password,
+        ios,
+        filter,
+        datastore
+):
+    with manager.connect(
+            host=host,
+            port=port,
+            username=username,
+            password=password,
+            hostkey_verify=False,
+            device_params={'name': ios}
+    ) as m:
+        netconfReply = m.edit_config(
+            config=filter,
+            target=datastore
+        )
         # Make returned XML data more human-readable
         temp = xml.dom.minidom.parseString(str(netconfReply.xml))
         new_xml = temp.toprettyxml()
--- xrTestScript.py
+++ xrTestScript.py
@@ -1,29 +1,31 @@
 import netconfActions
 from pathlib import Path
 
-# # Get Capabilities
-# netconfActions.netconfGetCapabilities(
-#         'sandbox-iosxr-1.cisco.com',
-#         '830',
-#         'admin',
-#         'C1sco12345',
-#         'iosxr'
-# )
+# Script mainly for use in DEVNET Sandbox XR 8000 Notebooks lab environment
 
-# Path to NETCONF Filter
-filterPath = Path.home()/'pyProjects'/'projects'/'iosXrLab'/'netconfFilter.xml'
-
-# Import filter into script
-xmlFilter = filterPath.read_text()
-
-# Get interface data
-print(
-    netconfActions.netconfGet(
+# Get Capabilities
+netconfActions.netconfGetCapabilities(
         'sandbox-iosxr-1.cisco.com',
         '830',
         'admin',
         'C1sco12345',
-        'iosxe',
-        filter=xmlFilter
-    )
+        'iosxr'
 )
+#
+# # Path to NETCONF Filter
+# filterPath = Path.home()/'pyProjects'/'projects'/'iosXrLab'/'netconfFilter.xml'
+#
+# # Import filter into script
+# xmlFilter = filterPath.read_text()
+#
+# # Get interface data
+# print(
+#     netconfActions.netconfGet(
+#         'sandbox-iosxr-1.cisco.com',
+#         '830',
+#         'admin',
+#         'C1sco12345',
+#         'iosxe',
+#         filter=xmlFilter
+#     )
+# )
