b2bba7ee3801ac74fc357d2d78432e599c61b705
slarrarte <100368309+slarrarte@users.noreply.github.com>
2024-10-01 18:59:18 -0400

    Update xrInterfaceInventory.py

--- xrInterfaceInventory.py
+++ xrInterfaceInventory.py
@@ -3,13 +3,13 @@
 # in your network (as many nodes as you list)
 from netconfActions import netconfGet as get
 from getpass import getpass
-import xmltodict, json
+import xmltodict, csv
 
-# Path to document where inventory data will be written to (JSON formatted)
+# Path to csv file
 database_path = ''
 
 # List all the router IPs that you'd like included in the data gathering
-router_loopbacks = ['']
+router_loopbacks = []
 
 # For security purposes, username and password will be entered at user input prompt
 enter_username = input('Username: \n')
@@ -27,22 +27,75 @@ xml_filter = """
 </filter>
 """
 
-# Import interfaceDatabase.json into program as a dictionary to be modified
-with open(database_path, 'r') as json_data:
-    dict_data = json.load(json_data)
+# List of interface data to be written to csv
+interface_data_list = [
+    ['Hostname', 'Interface', 'IP', 'Mask', 'VRF', 'Description']
+]
 
+# <get> each router in router_loopbacks
 for router in router_loopbacks:
-    router_response = xmltodict.parse(
-        get(
-            host=router,
-            port="830",
-            username=enter_username,
-            password=enter_password,
-            ios="iosxr",
-            filter=xml_filter
+    # Exception handling for when a node is unreachable
+    try:
+        router_response = xmltodict.parse(
+            get(
+                host=router,
+                port="830",
+                username=enter_username,
+                password=enter_password,
+                ios="iosxr",
+                filter=xml_filter
+            )
         )
-    )
-    dict_data[router_response['rpc-reply']['data']['hostname']['system-network-name']] = router_response['rpc-reply']['data']['interface-configurations']['interface-configuration']
-database_doc = open(database_path, 'w')
-database_doc.write(json.dumps(dict_data, indent=3))
-database_doc.close()
+        # Parsing per interface on router
+        for interface in range(len(router_response['rpc-reply']['data']['interface-configurations']['interface-configuration'])):
+            # Sub-list to be appended to interface_data_list
+            host_list = []
+            # Hostname will be added to host_list[0] only if this is the first interface of said host;
+            # otherwise, host_list[0] will be empty.
+            if interface == 0:
+                host_list.append(router_response['rpc-reply']['data']['hostname']['system-network-name'])
+            else:
+                host_list.append('')
+            # Interface
+            host_list.append(
+                router_response['rpc-reply']['data']['interface-configurations']['interface-configuration'][interface]['interface-name']
+            )
+            # IP
+            try:
+                host_list.append(
+                    router_response['rpc-reply']['data']['interface-configurations']['interface-configuration'][interface]['ipv4-network']['addresses']['primary']['address']
+                )
+            except:
+                host_list.append('')
+            # Mask
+            try:
+                host_list.append(
+                    router_response['rpc-reply']['data']['interface-configurations']['interface-configuration'][interface]['ipv4-network']['addresses']['primary']['netmask']
+                )
+            except:
+                host_list.append('')
+            # VRF
+            try:
+                host_list.append(
+                    router_response['rpc-reply']['data']['interface-configurations']['interface-configuration'][interface]['vrf']['#text']
+                )
+            except:
+                host_list.append('')
+            # Description
+            try:
+                host_list.append(
+                    router_response['rpc-reply']['data']['interface-configurations']['interface-configuration'][interface]['description']
+                )
+            except:
+                host_list.append('')
+            # Write sublist to interface_data_list
+            interface_data_list.append(host_list)
+    # Writes IP as hostname with blank arguments if router is unreachable
+    except:
+        interface_data_list.append([router, '', '', '', '', ''])
+        continue
+
+# Import csv file into program and modify it with gathered NETCONF data
+with open(database_path, 'w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerows(interface_data_list)
